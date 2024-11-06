d38c26c8ec46430b12d3926a63dcb3b1edff309e
egrizq <rizq.ramadhan17@gmail.com>
2024-11-06 07:36:32 +0700

    feat: implement repo for database query

---
+++ src/repository.py
@@ -0,0 +1,35 @@
+from sqlalchemy import text
+from database import db
+
+
+def check_username(username):
+    sql = text("SELECT username FROM users WHERE username = :username LIMIT 1")
+    result = db.session.execute(sql, {"username": username})
+
+    user = result.fetchone()
+
+    is_exist = user is not None
+
+    return is_exist
+
+
+def insert_account(name, username, hash_password):
+    sql = text(
+        "insert into users (name, username, password) values (:name, :username, :password)"
+    )
+    db.session.execute(
+        sql,
+        {"name": name, "username": username, "password": hash_password},
+    )
+    db.session.commit()
+
+    return "register successfully!"
+
+
+def get_hashed_password(username):
+    sql = text("SELECT password FROM users WHERE username = :username LIMIT 1")
+    result = db.session.execute(sql, {"username": username})
+
+    row = result.fetchone()
+
+    return row[0] if row else None
